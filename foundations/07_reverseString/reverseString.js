bb1e2c289d4fb94a179e146500f2339edec638a5
jyap36 <jyap36@gatech.edu>
2026-02-16 13:25:34 +0800

    reverse string attempt

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,9 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    reverse_string = '';
+    for (let i=str.length-1; i>=0; i--) {
+        reverse_string += str[i]
+    }
+    return reverse_string
 
 };
 
