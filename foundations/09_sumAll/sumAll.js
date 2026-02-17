950eae9d2f7f705527743458a615fbfe338c08a1
jyap36 <jyap36@gatech.edu>
2026-02-17 10:38:37 +0800

    sumall attempt

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,30 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (max < 0 || min < 0) {
+        return 'ERROR';
+    }
+
+    if (typeof min !== "number" || typeof max !== "number") {
+        return 'ERROR';
+    }
+
+    if (!Number.isInteger(min) || !Number.isInteger(min)) {
+        return 'ERROR';
+    }
+    
+    else if (min < max) {
+        let sum = 0
+        for (let i = min; i<=max; i++) {
+            sum += i
+        }
+        return sum;
+    }
+    else if (max < min) {
+        let sum = 0
+        for (let i = max; i<=min; i++) {
+            sum += i
+        }
+        return sum;
+    }
 
 };
 
